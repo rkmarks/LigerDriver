7dab890768a9e72f2c8c5e65dce9b08721dbfa40
Daniel Essig <dessig@godaddy.com>
2015-04-10 00:25:53 -0700

    Fixed various things - left comments

--- apps/assets/js/script.js
+++ apps/assets/js/script.js
@@ -1,15 +1,30 @@
 var sharedCount = 0;
 var serverCount = 0;
 
-		var changeCount = function(type,amount){
-		    switch(type){
-		        case 'server':
-		            serverCount+=amount;
-		            document.getElementById('serverQty').value = serverCount;
-		            break;
-		        case 'shared':
-		            sharedCount+=amount;
-		            document.getElementById('sharedQty').value = sharedCount;
-		            break;
-		    }
-		}
+var changeCount = function(type,amount){
+	switch(type){
+    case 'server':
+			/*
+      serverCount+=amount;
+      document.getElementById('serverQty').value = serverCount;
+			*/
+			$('#serverQty').val(serverCount+=amount);
+      break;
+    case 'shared':
+			/*
+      sharedCount+=amount;
+      document.getElementById('sharedQty').value = sharedCount;
+			*/
+			$('#sharedQty').val(sharedCount+=amount);
+      break;
+  }
+}
+
+
+$(document).ready(function(){
+  $('form').click(function(e) {
+    console.log(e);
+    e.preventDefault(); // stop your form from refreshing the page
+  });
+
+});

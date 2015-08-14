70f4647a8f9985a111923364432e485585b63652
rkmarks <rmarkins@gmail.com>
2015-08-14 10:48:18 -0700

    modified js for new managed can

--- apps/assets/js/script.js
+++ apps/assets/js/script.js
@@ -1,30 +1,6 @@
-var sharedCount = 0;
-var serverCount = 0;
+var controlP =
+var cpanelC =
+var pleskC =
+if (controlP == true){
 
-var changeCount = function(type,amount){
-	switch(type){
-    case 'server':
-			/*
-      serverCount+=amount;
-      document.getElementById('serverQty').value = serverCount;
-			*/
-			$('#serverQty').val(serverCount+=amount);
-      break;
-    case 'shared':
-			/*
-      sharedCount+=amount;
-      document.getElementById('sharedQty').value = sharedCount;
-			*/
-			$('#sharedQty').val(sharedCount+=amount);
-      break;
-  }
 }
-
-
-$(document).ready(function(){
-  $('form').click(function(e) {
-    console.log(e);
-    e.preventDefault(); // stop your form from refreshing the page
-  });
-
-});

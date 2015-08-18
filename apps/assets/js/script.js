dfac46f30b9baf3f120c1fc878b7bc506ab9d93e
rkmarks <rmarkins@gmail.com>
2015-08-17 17:17:23 -0700

    Form update
    another update

--- apps/assets/js/script.js
+++ apps/assets/js/script.js
@@ -1,6 +1,12 @@
-var controlP =
-var cpanelC =
-var pleskC =
-if (controlP == true){
-
+var pleskC = document.getElementById('Plesk');
+var cpanelC = document.getElementById('cPanel')
+var ipAddr = document.getElementById('ipAddress')
+function emailFunct (){
+	if (cpanelC == true){
+		var cpstring = "Dear Sir or Madam,\n" + ""
+		document.getElementsByTagName('textarea').value = cpstring;
+	} else if (pleskC == true){
+		var plkstring = ""
+		document.getElementsByTagName('textarea').value = plkstring;
+	}
 }

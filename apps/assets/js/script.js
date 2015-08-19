f588d049fcc2d764a63d79ab2b1c3e283e52d317
rkmarks <rmarkins@gmail.com>
2015-08-18 22:25:20 -0700

    form update

--- apps/assets/js/script.js
+++ apps/assets/js/script.js
@@ -1,12 +1,12 @@
 var pleskC = document.getElementById('Plesk');
 var cpanelC = document.getElementById('cPanel')
 var ipAddr = document.getElementById('ipAddress')
-function emailFunct (){
+function emailFunct (cpanelC, pleskC){
 	if (cpanelC == true){
-		var cpstring = "Dear Sir or Madam,\n" + ""
+		var cpstring = "Dear Sir or Madam,\r\n";
 		document.getElementsByTagName('textarea').value = cpstring;
 	} else if (pleskC == true){
-		var plkstring = ""
+		var plkstring = "Dear Sir or Madam,\r\n";
 		document.getElementsByTagName('textarea').value = plkstring;
 	}
 }

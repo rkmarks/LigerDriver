fbbb5149e9e5243c791974c41fa6e0f1183ad5bf
rkmarks <rmarkins@gmail.com>
2015-08-20 09:34:19 -0700

    small update

--- apps/assets/js/script.js
+++ apps/assets/js/script.js
@@ -3,10 +3,13 @@ var cpanelC = document.getElementById('cPanel');
 var ipAddr = document.getElementById('ipAddress');
 function emailFunct (cpanelC, pleskC){
 	if (cpanelC == true){
-		var cpstring = "Dear Sir or Madam,\r\n";
+		var cpstring = "Dear Sir or Madam,\r\n" + ipAddr;
 		document.getElementsByTagName('textarea').value = cpstring;
 	} else if (pleskC == true){
-		var plkstring = "Dear Sir or Madam,\r\n";
+		var plkstring = "Dear Sir or Madam,\r\n" + ipAddr;
 		document.getElementsByTagName('textarea').value = plkstring;
+	}else {
+		alert("Please choose a control panel");
 	}
+
 }

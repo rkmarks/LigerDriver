1d0619b6cbe4f1abd06be0819bd6f0ef365ed0f4
rkmarks <rmarkins@gmail.com>
2015-08-20 09:50:35 -0700

    form update
    updated javascript and html page

--- apps/assets/js/script.js
+++ apps/assets/js/script.js
@@ -1,15 +1,18 @@
-var pleskC = document.getElementById('Plesk');
-var cpanelC = document.getElementById('cPanel');
 var ipAddr = document.getElementById('ipAddress');
-function emailFunct (cpanelC, pleskC){
-	if (cpanelC == true){
-		var cpstring = "Dear Sir or Madam,\r\n" + ipAddr;
-		document.getElementsByTagName('textarea').value = cpstring;
-	} else if (pleskC == true){
-		var plkstring = "Dear Sir or Madam,\r\n" + ipAddr;
-		document.getElementsByTagName('textarea').value = plkstring;
-	}else {
-		alert("Please choose a control panel");
-	}
 
+function emailFunct() {
+  if (ipAddr == " ") {
+    alert('Please enter an IP address');
+    if (document.getElementById('controlPdrop').value == "cPanel") {
+      var cpstring = "Dear Sir or Madam,\r\n" + ipAddr;
+      document.getElementsByTagName('textarea').value = cpstring;
+    } else if (document.getElementById('controlPdrop').value == "Plesk") {
+      var plkstring = "Dear Sir or Madam,\r\n" + ipAddr;
+      document.getElementsByTagName('textarea').value = plkstring;
+    } else {
+      alert("Please choose a control panel");
+    }
+  } else {
+
+  }
 }

b74f3d4169476c57fca70a0f8b73fbe2cf9c0be3
rkmarks <rmarkins@gmail.com>
2015-09-02 07:22:47 -0700

    added reset button to form

--- apps/cansScript.js
+++ apps/cansScript.js
@@ -1,17 +1,22 @@
 emailFunct = function(ipAddr, usrNme) {
   var ipAddr = document.getElementById('ipAddress').value;
   if (!ipAddr) {
-    alert("Please enter an IP address and your name.");
+    alert("Please enter an IP address");
   } else {
     if (document.getElementById('controlPdrop').value == "cPanel") {
-      var cpStrg = "Valued Customer,\n\tThank you for your purchase of Managed Services." + ipAddr;
-      document.getElementById('canHtml').innerHTML = cpStrg;
+      var cpStrg = "Valued Customer,\n\t" + "Thank you for your purchase of Managed Services." + ipAddr;
+      document.getElementById('txtCan').value = cpStrg;
     }
     else if (document.getElementById('controlPdrop').value == "Plesk") {
-        var plkStrg = "Valued Customer,\n\tThank you for your purchase of Managed Services." + ipAddr;
-        document.getElementById('canHtml').innerHTML = plkStrg;
-      } else {
+        var plkStrg = "Valued Customer,\n\n\t" + "Thank you for your purchase of Managed Services." + ipAddr;
+        document.getElementById('txtCan').value = plkStrg;
+      }
+      else {
         alert("Please choose a control panel");
       }
     }
 };
+
+resetFunct = function() {
+  document.getElementById('canForm').reset();
+}

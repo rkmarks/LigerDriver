5fb6568c4fc5eb0cd014c4494234735b13ba062d
rkmarks <rmarkins@gmail.com>
2015-09-02 06:48:14 -0700

    Form now working properly
    will need to get correct text to update on from. testing on live site

--- apps/cansScript.js
+++ apps/cansScript.js
@@ -1,17 +1,17 @@
-var ipAddr = document.getElementById('ipAddress');
-
-var emailFunct = function() {
-  if (ipAddr == " ") {
-    alert('Please enter an IP address');
+emailFunct = function(ipAddr, usrNme) {
+  var ipAddr = document.getElementById('ipAddress').value;
+  if (!ipAddr) {
+    alert("Please enter an IP address and your name.");
   } else {
     if (document.getElementById('controlPdrop').value == "cPanel") {
-      var cpstring = "Dear Sir or Madam,\r\n" + ipAddr;
-      document.getElementsByTagName("p").value = cpstring;
-    } else if (document.getElementById('controlPdrop').value == "Plesk") {
-      var plkstring = "Dear Sir or Madam,\r\n" + ipAddr;
-      document.getElementsByTagName("p").value = plkstring;
-    } else {
-      alert("Please choose a control panel");
+      var cpStrg = "Valued Customer,\n\tThank you for your purchase of Managed Services." + ipAddr;
+      document.getElementById('canHtml').innerHTML = cpStrg;
     }
-  }
+    else if (document.getElementById('controlPdrop').value == "Plesk") {
+        var plkStrg = "Valued Customer,\n\tThank you for your purchase of Managed Services." + ipAddr;
+        document.getElementById('canHtml').innerHTML = plkStrg;
+      } else {
+        alert("Please choose a control panel");
+      }
+    }
 };

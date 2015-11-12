f71589b10074cb7230332cb4918cf8242e5c80b6
Rick Markins <rmarkins@G1WSHA-K004MS.jomax.paholdings.com>
2015-11-11 21:00:50 -0700

    updated the favicon

--- apps/assets/js/cansScript.js
+++ apps/assets/js/cansScript.js
@@ -24,44 +24,5 @@ resetFunct = function() {
   document.getElementById('canForm').reset();
 };
 
-///copytoClipboard function
-copytoClipboard = function() {
-  var aux = document.createElement("input");
-  aux.setAttribute("value", document.getElementById('id'))
-};
-
-//create cookie for repeated use
-function setCookie(cname, cvalue, exdays) {
-  var d = new Date();
-  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-  var expires = "expires=" + d.toGMTString();
-  document.cookie = cname + "=" + cvalue + "; " + expires;
-};
-//
-function getCookie(cname) {
-  var name = cname + "=";
-  var ca = document.cookie.split(';');
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') c = c.substring(1);
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-};
-
-//checks for cookie if already created
-function checkCookie() {
-  var user = getCookie("username");
-  if (user != "") {
-    alert("Welcome again " + user);
-  } else {
-    user = prompt("Please enter your first name and last name initial:", "");
-    if (user != "" && user != null) {
-      setCookie("username", user, 30);
-    }
-  }
-};
-
+document.cookie ="username=Rick M."
 var agentName = document.cookie;

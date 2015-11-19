abae4f9e133a717d958c2778c0638448e83d240c
Rick Markins <rmarkins@G1WSHA-K004MS.jomax.paholdings.com>
2015-11-18 22:58:24 -0700

    loader script
    made initial location for resources and comments

--- apps/LigerTools/loader.js
+++ apps/LigerTools/loader.js
@@ -1,3 +1,17 @@
-functionName: function(arguments) {
-  // body...
-}
+/*
+* NAME: LigerDriver loader
+* FILE: loader.js
+* AUTHOR: Rick Markins
+* PURPOSE: Primary load file for the tools toolbar and the resources needed to run
+*/
+
+console.log('[LIGER LOADED]');
+console.log('For assistance with LIGERTOOLS please email rmarkins@');
+
+/*==INIT=========================*/
+
+//base location of all app resources
+var initURL = '//ligerdriver.xyz/apps/LigerTools/resources';
+
+//location to test Liger
+var testURL = '//ligerdriver.xyz/apps/LigerTools'

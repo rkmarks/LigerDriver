249286ce198106bb4310163c2836a03ba5645097
Rick Markins <rmarkins@G1WSHA-K004MS.jomax.paholdings.com>
2015-11-18 22:40:56 -0700

    fixing for chrome

--- apps/LigerTools/ligerT.user.js
+++ apps/LigerTools/ligerT.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name LigerTools
-// @namespace   http://ligerdriver.xyz/apps/ligerTools/
-// @icon        http://ligerdriver.xyz/assets/images/Liger_logo.png
-// @downloadURL http://ligerdriver.xyz/apps/LigerTools/ligerT.user.js
+// @name        LigerTools
+// @namespace   https://ligerdriver.xyz/apps/LigerTools/
+// @icon        https://ligerdriver.xyz/assets/images/Liger_logo.png
+// @downloadURL https://ligerdriver.xyz/apps/LigerTools/ligerT.user.js
 // @description Multi-Tool for Managed Services
 // @include     *://toolzilla*
 // @include     *://crm*
@@ -16,6 +16,6 @@
 (function() {
   var script = document.createElement('script');
   script.type = 'text/javascript';
-  script.src = 'http://ligerdriver.xyz/apps/LigerTools/loader.js';
+  script.src = 'https://ligerdriver.xyz/apps/LigerTools/loader.js';
   document.getElementsByTagName('head')[0].appendChild(script);
 })();

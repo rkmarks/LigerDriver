1cd3564186a0df1b7cf3abc7af696e66540c6013
rkmarks <rmarkins@gmail.com>
2015-11-25 17:22:06 -0700

    updated app and removed blank page open

--- apps/LigerTools/liger.user.js
+++ apps/LigerTools/liger.user.js
@@ -1,21 +1,18 @@
 // ==UserScript==
 // @name        LigerTools
-// @namespace   https://ligerdriver.xyz/apps/LigerTools/
+// @namespace   liger_tools
 // @icon        https://ligerdriver.xyz/assets/images/Liger_logo.png
-// @downloadURL https://ligerdriver.xyz/apps/LigerTools/liger.user.js
 // @description Multi-Tool for Managed Services
 // @include     *://toolzilla*
 // @include     *://crm*
 // @include     *://iris*
 // @include     *://hs*
-// @version     0.1.0
+// @version     0.1.5
 // @grant       none
-// @copyright   2015+, Rick Markins (rmarkins@)
 // ==/UserScript==
 
-(function() {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'https://ligerdriver.xyz/apps/LigerTools/loader.js';
-  document.getElementsByTagName('head')[0].appendChild(script);
-})();
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://ligerdriver.xyz/apps/LigerTools/loader.js';
+document.getElementsByTagName('head')[0].appendChild(script);
+

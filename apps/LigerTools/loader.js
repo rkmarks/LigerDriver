2286fdd422088548dcaebb61f4d513a088fc220b
Rick Markins <rmarkins@G1WSHA-K004MS.jomax.paholdings.com>
2015-11-25 17:24:36 -0700

    start of the liger JS loader

--- apps/LigerTools/loader.js
+++ apps/LigerTools/loader.js
@@ -15,3 +15,35 @@ var initURL = '//ligerdriver.xyz/apps/LigerTools/resources';
 
 //location to test Liger
 var testURL = '//ligerdriver.xyz/apps/LigerTools'
+
+
+var apps = {
+
+/* This is where all the objects are stored for each page this toolbar will exist
+* Level 1 = Object name for the apps
+* Level 2 = objects of where they apply
+* Level 3 = objects containing arrays of URL, resources and settings
+*/
+
+//-Liger--------------------------------------------
+Liger: {
+  allTools: {
+    URLs: [
+    '//'
+  ],
+    Resources: [
+    ],
+    Settings: {
+      Names: [
+        CRM: {
+
+        },
+        HS: {
+
+        },
+        
+      ]
+    }
+  }
+}
+}

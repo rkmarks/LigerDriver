8b5a6450274f0c5adf9fc5a1f01f39ce741a2862
Richard Markins <rmarkins@godaddy.com>
2017-07-19 14:19:01 -0700

    modifying app to be a react app

---
+++ client/components/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div style={{textAlign: 'center'}}>
+                <h1>Hello world</h1>
+            </div>
+        );
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+/*
+    ./webpack.config.js
+ */
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+     template: './index.html',
+     filename: 'index.html',
+     inject: 'body'
+});
+
+module.exports = {
+    context: __dirname + '/client',
+    entry: './index.js',
+    output: {
+        path: __dirname + '/build',
+        filename: 'bundle.js'
+    },
+    module: {
+         loaders: [
+             { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+             { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
+             { test: /\.scss$/, loaders: ['style-loader', 'css-loader', 'sass-loader'], exclude: /node_modules/ }
+         ]
+    },
+    plugins: [HtmlWebpackPluginConfig]
+};
